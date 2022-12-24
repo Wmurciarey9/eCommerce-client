cc07be9fdf514498471c1284706b389d47a02c29
Wilson Murcia Rey <wmurciarey9@gmail.com>
2022-12-24 13:34:23 -0500

    testing the back end connection to deploy

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,10 +1,12 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux";
 import { publicRequest } from "../redux/requestMethods";
 
+const url = "https://ecommerce.adaptable.app/";
+
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
   try {
-    const res = await publicRequest.post("/auth/login", user);
+    const res = await publicRequest.post(url + "/auth/login", user);
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
