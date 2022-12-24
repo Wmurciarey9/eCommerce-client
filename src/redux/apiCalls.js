c3393c1f2f4d888fa1cacd60ffb6477b9c7850d6
Wilson Murcia Rey <wmurciarey9@gmail.com>
2022-12-24 14:04:08 -0500

    fixing errors

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,12 +1,10 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux";
 import { publicRequest } from "../redux/requestMethods";
 
-const url = "https://ecommerce.adaptable.app/";
-
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
   try {
-    const res = await publicRequest.post(url + "/auth/login", user);
+    const res = await publicRequest.post("/auth/login", user);
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
